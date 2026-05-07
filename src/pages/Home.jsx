2b65498abee8c29ac3e7653f63defc0ce44c25e2
satyam kumar mishra <satyamkmishraa@gmail.com>
2026-05-07 15:13:47 +0530

    adding more Feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -483,10 +483,12 @@ const Home = () => {
                   ₹{room.price}
                   <span className="text-sm text-gray-400"> /month</span>
                 </span>
-
-                <button className="px-4 py-2 text-sm rounded-full bg-indigo-600 text-white hover:bg-indigo-700 transition">
-                  View
-                </button>
+                 <Button 
+            onClick={() => navigate("/rooms")}
+           className="px-4 py-2 text-sm rounded-full bg-indigo-600 text-white hover:bg-indigo-700 transition"
+          >
+            View
+          </Button>
               </div>
             </div>
 
